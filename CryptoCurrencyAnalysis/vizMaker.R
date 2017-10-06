7ff8c8cf7e3484746bc3170220fd94390bb3186d
Tyler Neumann <neumanntylers@gmail.com>
2017-10-05 20:49:42 -0400

    added some functional columns

--- CryptoCurrencyAnalysis/vizMaker.R
+++ CryptoCurrencyAnalysis/vizMaker.R
@@ -4,7 +4,7 @@ library(grid)
 library(gridExtra)
 library(quantmod)
 
-rm(bitcoin, ethereum, neo, litecoin, coins, coins2016)
+rm(bitcoin, ethereum, neo, litecoin, coins, coins2016, coins2017)
 
 # Make sure to set your working dir to this file's location
 # Session -> Set Wroking Directory -> To Source File Location
@@ -31,6 +31,10 @@ coins <- rbind(bitcoin, ethereum, neo, litecoin)
 coins[, MClose := Close / max(Close), by = currency]
 coins[, scaled := scale(Close), by = currency]
 coins[, Date := as.Date(Date, format="%b %d, %Y")]
+coins[, MonthYear := format(coins$Date, "%b %Y")]
+coins[, Volume := as.numeric(gsub(",", "", Volume))]
+coins[, AvgMntYrVolume := mean(Volume), by = MonthYear]
+names(coins) <- c("Date", "Open", "High", "Low", "Close", "Volume", "MarketCap", "currency", "MClose", "Scaled", "MonthYear", "AvgMntYrVolume")
 
 # coins[, Date := as.Date(Date, format="%Y %m, %d")]
 # coins2016 <- subset(coins, Date > as.Date("2016/01/01"))
@@ -40,7 +44,7 @@ ggplot(coins, aes(Date, Close, color = currency)) +
 ggplot(coins, aes(Date, MClose, color = currency)) +
   geom_smooth() +
   geom_line()
-ggplot(coins, aes(Date, scaled, color = currency)) +
+ggplot(coins, aes(Date, Scaled, color = currency)) +
   geom_smooth() +
   geom_line()
 
@@ -53,3 +57,10 @@ ggplot(coins2017, aes(Date, Close, color = currency)) +
 ggplot(coins2017, aes(Date, scaled, color = currency)) +
   geom_smooth() +
   geom_line()
+ggplot(coins2017, aes(Date, MClose, color = currency)) +
+  geom_smooth() +
+  geom_line()
+ggplot(coins2017, aes(DateMY, AvgVolMY, color = currency)) +
+  geom_bar(stat="identity")
+ggplot(coins2017, aes(DateMY, VolumeInt, color = currency)) +
+  geom_bar(data=subset(coins2017, currency=="bitcoin"), stat="identity")
