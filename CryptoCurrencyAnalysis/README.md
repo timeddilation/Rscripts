dd09baa0a28e030f2a8b6d08cbbbea0038137faf
Tyler Neumann <neumanntylers@gmail.com>
2017-10-07 00:17:13 -0400

    Read me update, and some plots

--- CryptoCurrencyAnalysis/README.md
+++ CryptoCurrencyAnalysis/README.md
@@ -1,1 +1,3 @@
-https://www.bfxdata.com/datadownload/
+[Hourly Data](https://www.bfxdata.com/datadownload/)
+
+[Variety of Timescales](https://bitcoincharts.com/charts), however, you need to copy the table out of HTML table. Probably want an extension or something to do that for you.
--- CryptoCurrencyAnalysis/btcAnalysis.R
+++ CryptoCurrencyAnalysis/btcAnalysis.R
@@ -5,12 +5,9 @@ library(gridExtra)
 library(quantmod)
 rm(bitcoinMarket, bitcoinChain, bitcoinHashes, btcDataSet, btcDataSet2)
 
-bitcoinMarket <-
-  fread("cryptocurrencypricehistory/bitcoin_price.csv")
-bitcoinChain <-
-  fread("btcTransactionData.csv")
-bitcoinHashes <-
-  fread("cryptocurrencypricehistory/bitcoin_dataset.csv")
+bitcoinMarket <- fread("btcDataTables/bitcoin_price.csv")
+bitcoinChain <- fread("btcDataTables/btcTransactionData.csv")
+bitcoinHashes <- fread("btcDataTables/bitcoin_dataset.csv")
 
 merge1 <- 
   merge(bitcoinChain, bitcoinHashes)
@@ -18,3 +15,30 @@ btcDataSet <-
   merge(merge1, bitcoinMarket)
 rm(merge1)
 
+btcDataSet[, Date := as.Date(Date, format="%Y-%m-%d")]
+
+ggplot(btcDataSet, aes(Date)) +
+  geom_point(aes(y=btcDataSet$Transactions), color = "red") +
+  geom_point(aes(y=btcDataSet$Close), color = "blue")
+ggplot(btcDataSet, aes(Date)) +
+  geom_smooth(aes(y=scale(btcDataSet$Transactions)), color = "red") +
+  geom_smooth(aes(y=scale(btcDataSet$Close)), color = "blue")
+ggplot(btcDataSet, aes(Date, Close)) +
+  geom_line()
+ggplot(btcDataSet, aes(Date)) +
+  geom_smooth(aes(y=scale(btcDataSet$btc_hash_rate)), color = "red") +
+  geom_smooth(aes(y=scale(btcDataSet$Close)), color = "blue")
+
+sub2017.btc <- btcDataSet[Date >= as.Date("2017/01/01")]
+
+ggplot(sub2017.btc, aes(Date)) +
+  geom_line(aes(y=scale(sub2017.btc$Transactions)), color = "red") +
+  geom_line(aes(y=scale(sub2017.btc$Transactions * sub2017.btc$Close)), color = "green") +
+  geom_line(aes(y=scale(sub2017.btc$Close)), color = "blue")
+
+ggplot(sub2017.btc, aes(Date)) +
+  geom_line(aes(y=scale(sub2017.btc$btc_hash_rate)), color = "red") +
+  geom_line(aes(y=scale(sub2017.btc$Close)), color = "blue")
+ggplot(sub2017.btc, aes(Date)) +
+  geom_smooth(aes(y=scale(sub2017.btc$btc_hash_rate)), color = "red") +
+  geom_smooth(aes(y=scale(sub2017.btc$Close)), color = "blue")
