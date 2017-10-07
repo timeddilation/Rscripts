3e69d4edf46abc8bd082bc49684ef1bdd2c94cbe
Tyler Neumann <neumanntylers@gmail.com>
2017-10-07 10:28:59 -0400

    Refactor script, much better

--- CryptoCurrencyAnalysis/hourlyDataCoinsViews.R
+++ CryptoCurrencyAnalysis/hourlyDataCoinsViews.R
@@ -5,43 +5,48 @@ library(gridExtra)
 library(quantmod)
 
 bitcoin <- fread("HourlyData/vwapHourlyBTCUSD.csv")
-names(bitcoin) <- c("Timestamp", "BtcDate", "BtcPrice", "BtcVolume")
+bitcoin$Date <- NULL
+bitcoin$Volume <- NULL
+names(bitcoin) <- c("Timestamp", "BtcPrice")
 
 litecoin <- fread("HourlyData/vwapHourlyLTCUSD.csv")
-names(litecoin) <- c("Timestamp", "LtcDate", "LtcPrice", "LtcVolume")
+litecoin$Date <- NULL
+litecoin$Volume <- NULL
+names(litecoin) <- c("Timestamp", "LtcPrice")
 
 ethereum <- fread("HourlyData/vwapHourlyETHUSD.csv")
-names(ethereum) <- c("Timestamp", "EtcDate", "EtcPrice", "EtcVolume")
+ethereum$Date <- NULL
+ethereum$Volume <- NULL
+names(ethereum) <- c("Timestamp", "EtcPrice")
 
-merge1 <- merge(bitcoin, litecoin, by= "Timestamp")
-coins <- merge(merge1, ethereum, by= "Timestamp")
-rm(merge1)
-coins2017 <- subset(coins, "Timestamp" >= 1483228800)
+coinsPre1 <- merge(bitcoin, litecoin, by= "Timestamp")
+coinsPre2 <- merge(coinsPre1, ethereum, by= "Timestamp")
+coins <- melt(coinsPre2, id="Timestamp")
+coins[, ScaledPrice := scale(value), by = variable]
+rm(coinsPre1, coinsPre2)
 
-# Bitcoin = Gold
-# 
 plot.my.biz1 <-  function(data){
-  print(ggplot(data, aes(Timestamp)) +
-    geom_line(aes(y=data$BtcPrice), color = "#FF9900") +
-    geom_line(aes(y=data$LtcPrice), color = "#C0C0C0") +
-    geom_line(aes(y=data$EtcPrice), color = "#000000") +
-    theme_classic())
+  print(ggplot(data, aes(Timestamp, value, colour=variable)) +
+          geom_line() +
+          labs(title = "Raw Data", y = "Price") +
+          theme_classic())
   
 }
 plot.my.biz2 <-  function(data){
-  print(ggplot(data, aes(Timestamp)) +
-    geom_line(aes(y=scale(data$BtcPrice)), color = "#FF9900") +
-    geom_line(aes(y=scale(data$LtcPrice)), color = "#C0C0C0") +
-    geom_line(aes(y=scale(data$EtcPrice)), color = "#000000") +
-    theme_classic())
+  print(ggplot(data, aes(Timestamp, Scaled, colour=variable)) +
+          geom_line() +
+          labs(title = "Scaled Data", y = "Price") +
+          theme_classic())
 }
 plot.my.biz3 <-  function(data){  
-  print(ggplot(data, aes(Timestamp)) +
-    geom_smooth(aes(y=scale(data$BtcPrice)), color = "#FF9900") +
-    geom_smooth(aes(y=scale(data$LtcPrice)), color = "#C0C0C0") +
-    geom_smooth(aes(y=scale(data$EtcPrice)), color = "#000000") +
-    theme_classic())
+  print(ggplot(data, aes(Timestamp, Scaled, colour=variable)) +
+          geom_smooth() +
+          labs(title = "Scaled Data", subtitle = "Smooth", y = "Price") +
+          theme_classic())
 }
+
+coins2017 <- subset(coins, Timestamp >= 1483228800)
+coins2017 <- coins2017[, Scaled := scale(value), by = variable]
 
 plot.my.biz1(coins)
 
@@ -59,11 +64,22 @@ first90d2017 <- 1483228800 + 7776000
 first180d2017 <- 1483228800 + 15552000
 
 coinsLastWeek <- subset(coins, Timestamp >= lastTradeWeek)
+coinsLastWeek <- coinsLastWeek[, Scaled := scale(value), by = variable]
+
 coinsLast60 <- subset(coins, Timestamp >= lastTrade60)
+coinsLast60 <- coinsLast60[, Scaled := scale(value), by = variable]
+
 coinsLast180 <- subset(coins, Timestamp >= lastTrade180)
+coinsLast180 <- coinsLast180[, Scaled := scale(value), by = variable]
+
 coinsFirst60d2017 <- subset(coins, Timestamp >= first2017Trade & Timestamp <= first60d2017)
+coinsFirst60d2017 <- coinsFirst60d2017[, Scaled := scale(value), by = variable]
+
 coinsFirst90d2017 <- subset(coins, Timestamp >= first2017Trade & Timestamp <= first90d2017)
+coinsFirst90d2017 <- coinsFirst90d2017[, Scaled := scale(value), by = variable]
+
 coinsFirst180d2017 <- subset(coins, Timestamp >= first2017Trade & Timestamp <= first180d2017)
+coinsFirst180d2017 <- coinsFirst180d2017[, Scaled := scale(value), by = variable]
 
 plot.my.biz1(coinsLastWeek)
 plot.my.biz2(coinsLastWeek)
@@ -88,3 +104,4 @@ plot.my.biz3(coinsFirst90d2017)
 plot.my.biz1(coinsFirst180d2017)
 plot.my.biz2(coinsFirst180d2017)
 plot.my.biz3(coinsFirst180d2017)
+ggsave("plotTest.pdf", plot = last_plot(), device = "pdf", path = "plots/")
